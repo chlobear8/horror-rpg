7b66022fe284127c408b9e922b7b8fb71c1460a6
Chloe ONeil <chloesporri@yahoo.com>
2022-12-27 10:44:26 -0800

    pass second test

--- __tests__/characters.tests.js
+++ __tests__/characters.tests.js
@@ -1,9 +1,13 @@
-import { TestScheduler } from 'jest';
-import {Character} from '../src/js/horror.js';
+import Character from '../src/character';
 
 describe('Character', () => {
   test('should create a character object', () => {
     const player = new Character();
     expect(player).toBeInstanceOf(Character);
+  });
+
+  test('should set properties on class', () => {
+    const player = new Character(4, 4, 4);
+    expect(player.health).toEqual(4);
   });
 })
