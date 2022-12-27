0536912a4d070a02e5259943f8d24bcdf4c7092c
Chloe ONeil <chloesporri@yahoo.com>
2022-12-27 14:16:47 -0800

    pass third test for levelUp

--- src/character.js
+++ src/character.js
@@ -9,5 +9,8 @@ export default class Character extends Battleable {
   levelUp() {
     this.level += 1;
     this.damage +=1;
+    if (this.level % 2 === 0) {
+      this.specialTimes += 1;
+    }
   }
 }
