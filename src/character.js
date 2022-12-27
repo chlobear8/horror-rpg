9c8cfa36a5f9193847a31f14b27b9b1ddc1cb003
Chloe ONeil <chloesporri@yahoo.com>
2022-12-27 10:53:07 -0800

    pass third test with attack function

--- src/character.js
+++ src/character.js
@@ -1,5 +1,13 @@
 export default class Character {
-  constructor() {
-    
+  constructor(damage, special, health) {
+    this.damage = damage;
+    this.special = special;
+    this.specialTimes = 4;
+    this.health = health;
+  }
+
+  attack() {
+    return this.damage;
+
   }
 }
